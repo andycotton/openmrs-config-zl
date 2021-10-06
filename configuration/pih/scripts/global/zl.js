e2f5092bc139d296ab670355c5c120bffcad4a3e
Jean-Paul Louidor <56307966+louidorjp@users.noreply.github.com>
2021-10-06 13:29:18 -0400

    Uhm 5813 (#126)
    * UHM-5768 Transfer and referral separate
    
    * code refactoring
    
    * Add referral and transfer question to section-hiv-history and code refactoring
    
    * UHM-5813 - New informational questions on HIV Intake added
    
    * code refactoring

--- configuration/pih/scripts/global/zl.js
+++ configuration/pih/scripts/global/zl.js
@@ -157,3 +157,18 @@ function setUpDatepickerStartAndEndDateValidation(badDateInTheFutureMsg,badStart
     }
 }
 
+function setUpExpandableTransferAndReferralDetails(){
+    jq("#transfer-referral-spots").each(function(j, domEl){
+        jq("#transfer-referral-out-details").hide()
+        let elem=jq(domEl).find('input:checked').val()
+        if(elem){
+            jq("#transfer-referral-out-details").show()
+        }
+        jq(this).change(function (e){
+            if(e.target.value){
+            jq("#transfer-referral-out-details").show()
+            }
+        })
+    });
+}
+
