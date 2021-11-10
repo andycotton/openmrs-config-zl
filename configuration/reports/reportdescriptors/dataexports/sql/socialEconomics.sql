beba1d011ccf31a406bb757f28e4ca07f8c8dd6b
dmdesimone <ddesimone@pih.org>
2021-11-09 19:52:29 -0500

    add J9 fields
    add J9 fields

--- configuration/reports/reportdescriptors/dataexports/sql/socialEconomics.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/socialEconomics.sql
@@ -5,7 +5,7 @@ set session group_concat_max_len = 100000;
 
 set @encounter_type = encounter_type('Socio-economics');
 set @education = concept_from_mapping('CIEL','1712');
-set @educationAnswers = concept_name(@education, 'fr');
+set @educationAnswers = concept_name(@education, @locale);
 set @people_living_in_house = concept_from_mapping('CIEL','1474');
 set @number_of_children_living = concept_from_mapping('CIEL','1825');
 set @number_of_rooms = concept_from_mapping('CIEL','1475');
@@ -38,6 +38,7 @@ set @improved_drinking_water = concept_from_mapping('CIEL', '165497');
 set @electricity = concept_from_mapping('CIEL', '165498');
 set @inadequate_housing_materials = concept_from_mapping('CIEL', '165499');
 set @household_doesnot_own_assetts = concept_from_mapping('CIEL', '165500');
+set @locale = global_property_value('default_locale', 'en');
 
 create temporary table temp_social_econ
 (
@@ -54,6 +55,7 @@ create temporary table temp_social_econ
   age_at_enc double,
   education_answer_concept int,
   education varchar(50),
+  read_and_write varchar(50),
   people_living_in_house double,
   number_of_children_living double,
   number_of_rooms double,
@@ -69,8 +71,10 @@ create temporary table temp_social_econ
   transport_method_to_clinic varchar(50),
   cost_of_transport double,
   travel_time_to_clinic varchar(50),
+  access_to_transportation varchar(50),
   main_daily_activities_before_illness text,
   ability_to_perform_main_daily_activity_since_illness text,
+  currently_employed varchar(50),
   recieved_assistance text,
   recommended_assistance text,
   other_recommended_or_recieved_assistance_name text,
@@ -123,7 +127,10 @@ left join obs o on tsn.encounter_id = o.encounter_id and voided = 0
 set tsn.education_answer_concept = o.value_coded;
 
 update temp_social_econ tsn
-set education = concept_name(education_answer_concept, 'fr');
+set education = concept_name(education_answer_concept, @locale);
+
+update temp_social_econ tsn
+set read_and_write = obs_value_coded_list(tsn.encounter_id, 'PIH','13736',@locale);
 
 update temp_social_econ tsn
 left join obs o1 on tsn.encounter_id = o1.encounter_id and voided = 0 and concept_id = @people_living_in_house
@@ -139,44 +146,44 @@ set tsn.number_of_rooms = o3.value_numeric;
 
 update temp_social_econ tsn
 left join obs o4 on tsn.encounter_id = o4.encounter_id and voided = 0 and concept_id = @radio
-set tsn.radio = concept_name(o4.value_coded, 'fr');
+set tsn.radio = concept_name(o4.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o5 on tsn.encounter_id = o5.encounter_id and voided = 0 and concept_id = @television
-set tsn.television = concept_name(o5.value_coded, 'fr');
+set tsn.television = concept_name(o5.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o6 on tsn.encounter_id = o6.encounter_id and voided = 0 and concept_id = @fridge
-set tsn.fridge = concept_name(o6.value_coded, 'fr');
+set tsn.fridge = concept_name(o6.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o7 on tsn.encounter_id = o7.encounter_id and voided = 0 and concept_id = @bank_account
-set tsn.bank_account = concept_name(o7.value_coded, 'fr');
+set tsn.bank_account = concept_name(o7.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o8 on tsn.encounter_id = o8.encounter_id and voided = 0 and concept_id = @toilet
-set tsn.toilet = concept_name(o8.value_coded, 'fr');
+set tsn.toilet = concept_name(o8.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o9 on tsn.encounter_id = o9.encounter_id and voided = 0 and concept_id = @latrine
-set tsn.latrine = concept_name(o9.value_coded, 'fr');
+set tsn.latrine = concept_name(o9.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o10 on tsn.encounter_id = o10.encounter_id and voided = 0 and concept_id = @floor
-set tsn.floor = concept_name(o10.value_coded, 'fr');
+set tsn.floor = concept_name(o10.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o11 on tsn.encounter_id = o11.encounter_id and voided = 0 and concept_id = @roof
-set tsn.roof = concept_name(o11.value_coded, 'fr');
+set tsn.roof = concept_name(o11.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o12 on tsn.encounter_id = o12.encounter_id and voided = 0 and concept_id = @walls
-set tsn.walls = concept_name(o12.value_coded, 'fr');
+set tsn.walls = concept_name(o12.value_coded, @locale);
 
 -- TRANSPORTATION
 update temp_social_econ tsn
 left join obs o on tsn.encounter_id = o.encounter_id and voided = 0 and concept_id = @transport_method_to_clinic
-set tsn.transport_method_to_clinic = concept_name(o.value_coded, 'fr');
+set tsn.transport_method_to_clinic = concept_name(o.value_coded, @locale);
 
 update temp_social_econ tsn
 left join obs o1 on tsn.encounter_id = o1.encounter_id and voided = 0 and concept_id = @cost_of_transport
@@ -184,7 +191,10 @@ set tsn.cost_of_transport = o1.value_numeric;
 
 update temp_social_econ tsn
 left join obs o2 on tsn.encounter_id = o2.encounter_id and voided = 0 and concept_id = @travel_time_to_clinic
-set tsn.travel_time_to_clinic = concept_name(o2.value_coded, 'fr');
+set tsn.travel_time_to_clinic = concept_name(o2.value_coded, @locale);
+
+update temp_social_econ tsn
+set access_to_transportation = obs_value_coded_list(tsn.encounter_id, 'PIH','13746',@locale);
 
 -- DAILY ACTIVITIES
 update temp_social_econ tsn
@@ -195,11 +205,15 @@ update temp_social_econ tsn
 left join obs o1 on tsn.encounter_id = o1.encounter_id and voided = 0 and concept_id = @ability_to_perform_main_daily_activity_since_illness
 set tsn.ability_to_perform_main_daily_activity_since_illness = o1.value_text;
 
+update temp_social_econ tsn
+set currently_employed = obs_value_coded_list(tsn.encounter_id, 'PIH','3395',@locale);
+
+
 -- ASSISTANCE
 update temp_social_econ tsn
 left join (
 select encounter_id,  group_concat(name) names  from obs o join concept_name cn on cn.concept_id = o.value_coded and cn.voided = 0
-and o.voided = 0 and o.concept_id = @recieved_assistance and cn.locale = "en" and concept_name_type = "FULLY_SPECIFIED"
+and o.voided = 0 and o.concept_id = @recieved_assistance and cn.locale = @locale and concept_name_type = "FULLY_SPECIFIED"
 group by encounter_id
 ) o1 on tsn.encounter_id = o1.encounter_id
 set tsn.recieved_assistance = o1.names;
@@ -207,7 +221,7 @@ set tsn.recieved_assistance = o1.names;
 update temp_social_econ tsn
 left join (
 select encounter_id, o.concept_id concept_id,  value_coded, group_concat(name) names  from obs o join concept_name cn on cn.concept_id = o.value_coded and cn.voided = 0
-and o.voided = 0 and o.concept_id = @recommended_assistance and cn.locale = "en" and concept_name_type = "FULLY_SPECIFIED"
+and o.voided = 0 and o.concept_id = @recommended_assistance and cn.locale = @locale and concept_name_type = "FULLY_SPECIFIED"
 group by encounter_id
 ) o1 on tsn.encounter_id = o1.encounter_id
 set tsn.recommended_assistance = o1.names;
@@ -240,52 +254,52 @@ set tsn.socio_economic_assistance_comment = o.value_text;
 update temp_social_econ tsn
 left join
 obs o on o.encounter_id = tsn.encounter_id and o.voided = 0 and o.concept_id = @undernourishment
-set tsn.undernourishment = concept_name(o.value_coded, 'fr');
+set tsn.undernourishment = concept_name(o.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o1 on o1.encounter_id = tsn.encounter_id and o1.voided = 0 and o1.concept_id = @infant_mortality
-set tsn.infant_mortality = concept_name(o1.value_coded, 'fr');
+set tsn.infant_mortality = concept_name(o1.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o2 on o2.encounter_id = tsn.encounter_id and o2.voided = 0 and o2.concept_id = @completed_six_years_schooling
-set tsn.completed_six_years_schooling = concept_name(o2.value_coded, 'fr');
+set tsn.completed_six_years_schooling = concept_name(o2.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o3 on o3.encounter_id = tsn.encounter_id and o3.voided = 0 and o3.concept_id = @not_attending_school
-set tsn.not_attending_school = concept_name(o3.value_coded, 'fr');
+set tsn.not_attending_school = concept_name(o3.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o4 on o4.encounter_id = tsn.encounter_id and o4.voided = 0 and o4.concept_id = @cooks_with_dung_wood_charcoal_or_coal
-set tsn.cooks_with_dung_wood_charcoal_or_coal = concept_name(o4.value_coded, 'fr');
+set tsn.cooks_with_dung_wood_charcoal_or_coal = concept_name(o4.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o5 on o5.encounter_id = tsn.encounter_id and o5.voided = 0 and o5.concept_id = @household_sanitation_improvement
-set tsn.household_sanitation_improvement = concept_name(o5.value_coded, 'fr');
+set tsn.household_sanitation_improvement = concept_name(o5.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o6 on o6.encounter_id = tsn.encounter_id and o6.voided = 0 and o6.concept_id = @improved_drinking_water
-set tsn.improved_drinking_water = concept_name(o6.value_coded, 'fr');
+set tsn.improved_drinking_water = concept_name(o6.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o7 on o7.encounter_id = tsn.encounter_id and o7.voided = 0 and o7.concept_id = @electricity
-set tsn.electricity = concept_name(o7.value_coded, 'fr');
+set tsn.electricity = concept_name(o7.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o8 on o8.encounter_id = tsn.encounter_id and o8.voided = 0 and o8.concept_id = @inadequate_housing_materials
-set tsn.inadequate_housing_materials = concept_name(o8.value_coded, 'fr');
+set tsn.inadequate_housing_materials = concept_name(o8.value_coded, @locale);
 
 update temp_social_econ tsn
 left join
 obs o9 on o9.encounter_id = tsn.encounter_id and o9.voided = 0 and o9.concept_id = @household_doesnot_own_assetts
-set tsn.household_doesnot_own_assetts = concept_name(o9.value_coded, 'fr');
+set tsn.household_doesnot_own_assetts = concept_name(o9.value_coded, @locale);
 
 select
 tse.patient_id "patient_id",
@@ -300,6 +314,7 @@ encounter_id,
 encounter_date,
 age_at_enc,
 education,
+read_and_write,
 people_living_in_house,
 number_of_children_living,
 number_of_rooms,
@@ -315,8 +330,10 @@ walls,
 transport_method_to_clinic,
 cost_of_transport,
 travel_time_to_clinic,
+access_to_transportation,
 main_daily_activities_before_illness,
 ability_to_perform_main_daily_activity_since_illness,
+currently_employed,
 recieved_assistance,
 recommended_assistance,
 other_recommended_or_recieved_assistance_name,
