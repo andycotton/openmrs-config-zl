101bd676b3111228b68e963a54997039afabf67f
dmdesimone <ddesimone@pih.org>
2021-12-06 20:41:36 -0500

    Update pmtct_contacts.sql

--- configuration/pih/petl/jobs/sql/extractions/pmtct_contacts.sql
+++ configuration/pih/petl/jobs/sql/extractions/pmtct_contacts.sql
@@ -1,14 +1,19 @@
 -- set @locale = 'en';  -- uncomment for testing
 
-select program_workflow_state_id into @patientPregnant from program_workflow_state where concept_id = concept_from_mapping('PIH','1755');
+select program_id into @hivProgram from program where uuid = 'b1cb1fc1-5190-4f7a-af08-48870975dafc';
+
+select program_workflow_state_id into @patientPregnantState 
+from program_workflow_state where uuid = '5da5fa62-ef9f-4f15-9b58-d23136c2907b';
 
 drop temporary table if exists temp_contacts;
 
 create temporary table temp_contacts
 (
 patient_id int,
+zlemrid varchar(255),
 concept_obs_group_id int,
 age int,
+latest_patient_program_id int, 
 hiv_enrollment_date datetime,
 pmtct_initiation_date datetime,
 art_start_date datetime,
@@ -35,24 +40,34 @@ where o.voided = 0
 and concept_id = concept_from_mapping('PIH','13182')
 order by person_id, o.obs_datetime asc, obs_id asc;
 
+-- zl emr id
+update temp_contacts
+set zlemrid = zlemr(patient_id);
+
 -- current age
 update temp_contacts
 set age = current_age_in_years(patient_id);
 
--- hiv enrollment date from the latest enrollment date of the patient's hiv program
-update temp_contacts
-set hiv_enrollment_date = latestProgramEnrollmentDate(patient_id,'b1cb1fc1-5190-4f7a-af08-48870975dafc');
+-- populates latest patient program for HIV for each patient
+update temp_contacts t
+inner join patient_program pp on pp.patient_program_id =
+	(select patient_program_id from patient_program pp2 
+	where pp2.patient_id = t.patient_id
+	and pp2.program_id = @hivProgram
+	order by date_enrolled desc limit 1)
+set latest_patient_program_id = pp.patient_program_id,
+	hiv_enrollment_date = pp.date_enrolled ;
 
--- this pulls the pmtct initiation date, which is the start date of the patient state of "pregnant" for the patient's latest enrollment
+-- set pmtct initiation date to most recent start of patient pregant state for the patient program id
 update temp_contacts t 
-inner join 
-	(select pp.patient_id, pp.date_enrolled, max(start_date) as pregnant_start_date from patient_state ps
-	inner join patient_program pp on ps.patient_program_id = pp.patient_program_id 
-	and state = @patientPregnant
-	group by patient_id,pp.date_enrolled) s on s.patient_id = t.patient_id and s.date_enrolled = t.hiv_enrollment_date
-set	pmtct_initiation_date = s.pregnant_start_date;
+inner join patient_state ps on ps.patient_state_id =
+	(select patient_state_id from patient_state ps2
+	where ps2.patient_program_id = t.latest_patient_program_id
+	and ps2.state =  @patientPregnantState
+	order by ps2.start_date desc limit 1)
+set pmtct_initiation_date = ps.start_date ;
 
--- art start date 
+-- art start date as the start date of orders with order reason of hiv
 update temp_contacts t
 set art_start_date = orderReasonStartDate(t.patient_id,'PIH','11197');
 
@@ -66,6 +81,7 @@ set contact_hiv_status = obs_from_group_id_value_coded_list(t.concept_obs_group_
 
 select 
 patient_id,
+zlemrid,
 age,
 hiv_enrollment_date,
 pmtct_initiation_date,
