2f9ada4cdda1b236b21321106b66b1ed738e8f87
James Mbabazi <jmbabazi@pih.org>
2022-01-12 10:20:11 -0500

    removing temporary

--- configuration/pih/petl/jobs/sql/schemas/summary_db_restore.sql
+++ configuration/pih/petl/jobs/sql/schemas/summary_db_restore.sql
@@ -1,4 +1,4 @@
-CREATE TEMPORARY TABLE summary_db_restore (
+CREATE TABLE summary_db_restore (
 last_restored_patient_datapoint_created_on   DATETIME,
 last_restored_encounter_datapoint_created_on DATETIME,
 last_restored_obs_datapoint_created_on       DATETIME
