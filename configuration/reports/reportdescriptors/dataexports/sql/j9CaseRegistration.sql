abdb5544ac07e24d8ee9f6e9ff5a03ab7de97894
dmdesimone <ddesimone@pih.org>
2022-09-09 12:31:47 -0400

    Update j9CaseRegistration.sql

--- configuration/reports/reportdescriptors/dataexports/sql/j9CaseRegistration.sql
+++ configuration/reports/reportdescriptors/dataexports/sql/j9CaseRegistration.sql
@@ -36,11 +36,10 @@ create temporary table temp_J9_patients
     locality varchar(255),
     street_landmark varchar(255),
     j9_enrollment_date datetime
-)
-;
-drop TEMPORARY TABLE IF EXISTS temp_mch_pp;
-create TEMPORARY TABLE temp_mch_pp
-select * from patient_program pp
+);
+
+insert into temp_J9_patients (patient_id, patient_program_id, j9_enrollment_date)
+select pp.patient_id, pp.patient_program_id ,pp.date_enrolled  from patient_program pp
 where pp.patient_program_id =
          	(select pp2.patient_program_id from patient_program pp2
          	where pp2.patient_id = pp.patient_id 
@@ -48,12 +47,6 @@ where pp.patient_program_id =
          	and pp2.voided = 0  
          	and pp2.date_completed is null
          	order by pp2.date_enrolled desc limit 1);
-         	
--- load one row per patient in program
-insert into temp_J9_patients (patient_id, patient_program_id, j9_enrollment_date)
-Select pp.patient_id, pp.patient_program_id ,pp.date_enrolled 
-FROM temp_mch_pp pp 
-WHERE  pp.program_id = @matHealthProgram and pp.voided = 0  and pp.date_completed is NULL;
 
 create index temp_J9_patients_pi on temp_J9_patients(patient_id);
 
@@ -63,8 +56,8 @@ inner join person p on p.person_id = t.patient_id
 where p.dead = 1;
 
 -- load all relevant encounters (for use later to narrow down observations)
-drop temporary table if exists temp_obgyn_enc;
-create temporary table temp_obgyn_enc
+drop temporary table if exists temp_j9_enc;
+create temporary table temp_j9_enc
 select e.encounter_id  from encounter e
 inner join temp_J9_patients t on t.patient_id = e.patient_id 
 where e.encounter_type in ( @obgynEncId, @delEncId)
@@ -73,7 +66,7 @@ and e.voided = 0;
 -- uuid, age,  birthdate
 update temp_J9_patients t
 inner join person p on p.person_id = t.patient_id
-	set patient_uuid = p.uuid,
+	set t.patient_uuid = p.uuid,
 		t.birthdate = p.birthdate;
 
 update temp_J9_patients t
@@ -85,38 +78,45 @@ set t.J9_program = currentProgramState(t.patient_program_id,@MCH_treatment,'en')
 
 -- telephone number
 update temp_J9_patients t
-    left outer join person_attribute pa on pa.person_id = t.patient_id and pa.person_attribute_type_id = @tele
-        and pa.voided = 0
-set t.telephone = pa.value;
+set t.telephone = phone_number(t.patient_id);
 
 -- patient address
 update temp_J9_patients t
-    left outer join current_name_address cna on cna.person_id = t.patient_id
-set t.department = cna.department,
-    t.commune = cna.commune,
-    t.section_communal = cna.section_communal,
-    t.locality = cna.locality,
-    t.street_landmark = cna.street_landmark;
+set department = person_address_state_province(t.patient_id);
 
-   -- identifiers
+update temp_J9_patients t
+set commune = person_address_city_village(t.patient_id);
+
+update temp_J9_patients t
+set section_communal = person_address_three(t.patient_id);
+
+update temp_J9_patients t
+set locality = person_address_one(t.patient_id);
+
+update temp_J9_patients t
+set street_landmark = person_address_two(t.patient_id);
+
+-- identifiers
 update temp_J9_patients t
 set t.ZL_identifier =  zlEMR(t.patient_id);
 
 update temp_J9_patients t
 set t.Dossier_identifier =  dosId(t.patient_id);
 
--- patient names
+-- patient name
 update temp_J9_patients t
-    left outer join current_name_address cna on cna.person_id = t.patient_id
-set t.given_name = cna.given_name,
-    t.family_name = cna.family_name;
+set given_name = person_given_name(t.patient_id);
 
+update temp_J9_patients t
+set family_name = person_family_name(t.patient_id);
+
+   
 -- ------------- latest mothers group -------------------------
 DROP TEMPORARY TABLE IF EXISTS temp_mg_obs;
 create temporary table temp_mg_obs 
-select o.obs_id,  o.person_id, o.concept_id, o.value_text,o.obs_datetime 
+select o.obs_id, o.person_id, o.obs_datetime 
 from obs o
-inner join temp_obgyn_enc t on t.encounter_id = o.encounter_id
+inner join temp_j9_enc t on t.encounter_id = o.encounter_id
 where o.voided = 0
 and o.concept_id = @mothersGroup; 
 
@@ -132,9 +132,9 @@ set t.J9_group = o.value_text;
 -- ------------- latest expected delivery date -------------------------
 DROP TEMPORARY TABLE IF EXISTS temp_edd_obs;
 create temporary table temp_edd_obs 
-select o.obs_id, o.person_id, o.value_datetime, o.obs_datetime 
+select o.obs_id, o.person_id, o.obs_datetime 
 from obs o
-inner join temp_obgyn_enc t on t.encounter_id = o.encounter_id
+inner join temp_j9_enc t on t.encounter_id = o.encounter_id
 where o.voided = 0
 and o.concept_id = @edd;
 
@@ -150,9 +150,9 @@ set t.estimated_delivery_date = o.obs_datetime;
 -- ------------- latest actual delivery date -------------------------
 DROP TEMPORARY TABLE IF EXISTS temp_add_obs;
 create temporary table temp_add_obs 
-select o.obs_id, o.person_id, o.value_datetime, o.obs_datetime 
+select o.obs_id, o.person_id, o.obs_datetime 
 from obs o
-inner join temp_obgyn_enc t on t.encounter_id = o.encounter_id
+inner join temp_j9_enc t on t.encounter_id = o.encounter_id
 where o.voided = 0
 and o.concept_id = @add;
 
