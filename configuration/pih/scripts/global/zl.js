2064dedad84f9b83b390016d4f68da5c04c4a7bc
Mark Goodrich <mgoodrich@pih.org>
2023-02-16 13:27:46 -0800

    Revert "HAI-709: Valider le numéro de téléphone côté serveur de la personne de contact"
    This reverts commit 49d314554cf03a3dee50d5c07eed217fb8c91634.

--- configuration/pih/scripts/global/zl.js
+++ configuration/pih/scripts/global/zl.js
@@ -131,19 +131,17 @@ function setUpPhoneNumberRegex(badPhoneNumberMsg) {
 
     jq(this).change(function (e) {
       let phone = e.target.value;
-      if (phoneNumberRegex(phone)) {
+      if (phone.match(phoneNumberPattern().pattern1) || phone.match(phoneNumberPattern().pattern2) || phone.match(phoneNumberPattern().pattern3)) {
         jq(this).find('span').first().hide();
         jq(this).find('span').first().text('');
+        setButtonsDisabled(false);
       } else {
         jq(this).find('span').first().show();
         jq(this).find('span').first().text(badPhoneNumberMsg);
+        setButtonsDisabled(true); setUpPhoneNumberRegex
       }
     })
   })
-}
-
-function phoneNumberRegex(phone) {
-  return phone.match(phoneNumberPattern().pattern1) || phone.match(phoneNumberPattern().pattern2) || phone.match(phoneNumberPattern().pattern3);
 }
 
 function phoneNumberPattern() {
