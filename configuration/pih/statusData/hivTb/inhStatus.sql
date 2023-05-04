00f9acaeab0ed965bd0bd1b35f7342f32764dc14
Jean-Paul Louidor <jpaullouidor@gmail.com>
2023-05-04 14:22:10 -0400

    INH initiation/completion date on HIV Patient Summary (#238)
    * INH initiation/completion date on HIV Patient Summary
    
    * update
    
    * update section-hiv-state.xml
    
    * Keep the old concepts for the INH start date and end date
    
    * update concept ids and fixed sql

--- configuration/pih/statusData/hivTb/inhStatus.sql
+++ configuration/pih/statusData/hivTb/inhStatus.sql
@@ -1,23 +1,33 @@
 # This retrieves all of the data points needed to provide information as to the INH Prophylaxis for the patient
 
-select concept_from_mapping('CIEL', '78280') into @inh;
+select concept_from_mapping('PIH', '2739') into @currentIsoniazidConstructConceptId;
+select concept_from_mapping('PIH', '1501') into @isoniazidConstructConceptId;
 
-select      ifnull(o.scheduled_date, o.date_activated),
-            ifnull(o.date_stopped, o.auto_expire_date)
-            into @startDate, @endDate
-from        orders o
-where       o.patient_id = @patientId
+select      o.encounter_id into @encounterId
+from        obs o
+join        encounter e on e.encounter_id=o.encounter_id
+where       e.patient_id =@patientId
 and         o.voided = 0
-and         o.order_action != 'DISCONTINUE'
-and         o.concept_id = @inh
-order by    ifnull(o.scheduled_date, o.date_activated) desc
-limit 1
-;
+and         o.concept_id = @isoniazidConstructConceptId
+order by    o.obs_datetime desc
+limit 1;
+
+select      o.encounter_id into @currentEncounterId
+from        obs o
+join        encounter e on e.encounter_id=o.encounter_id
+where       e.patient_id =@patientId
+and         o.voided = 0
+and         o.concept_id = @currentIsoniazidConstructConceptId
+order by    o.obs_datetime desc
+limit 1;
+
+select obs_from_group_id_value_datetime(obs_group_id_of_value_coded(ifnull(@currentEncounterId,@encounterId), 'PIH',if(@currentEncounterId > 0,'2289','1282'), 'PIH','656'), 'PIH',if(@currentEncounterId > 0,'11131','1190')) into @startDate; 
+
+select obs_from_group_id_value_datetime(obs_group_id_of_value_coded(ifnull(@currentEncounterId,@encounterId), 'PIH',if(@currentEncounterId > 0,'2289','1282'), 'PIH','656'), 'PIH',if(@currentEncounterId > 0,'12748','1191')) into @endDate; 
 
 select
     date(@startDate) as startDate,
     date(@endDate) as endDate,
     if(@endDate > now(), 0, 1) as isCompleted,
     if(@startDate > now(), 'future', if(@endDate > now(), 'active', 'completed')) as status,
-    datediff(ifnull(@endDate, now()), @startDate)+1 as duration
-;
+    datediff(ifnull(@endDate, now()),@startDate)+1 as duration;
