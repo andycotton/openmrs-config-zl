e9ef15851b1f68250ffa8b7a05a4601448008956
Ing Louidor <jpaullouidor@gmail.com>
2023-06-20 11:37:53 -0400

    update lastDisposition.sql

--- configuration/pih/statusData/mentalHealth/lastDisposition.sql
+++ configuration/pih/statusData/mentalHealth/lastDisposition.sql
@@ -1,16 +1,6 @@
-select concept_from_mapping('PIH', '8620')
-into @dispositionCategory;
-
-
-
 # Get the latest value_coded with dispositionCategory 
-SELECT o.value_coded  into @valueCoded
-FROM obs o
-where o.person_id = @patientId
-and o.concept_id = @dispositionCategory
-and o.voided = 0
-order by o.obs_datetime desc
-limit 1;
 
-select
-    @valueCoded as disposition;
+select o.value_coded as disposition 
+from obs o
+where o.obs_id = latest_obs(@patientId, 'PIH', '8620')
+and o.voided = 0;
