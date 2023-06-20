5e840beefe679fbf544c2976c64aa5a8813e89e9
Ing Louidor <jpaullouidor@gmail.com>
2023-06-20 10:30:35 -0400

    Add program status to the mental health status section of the patient dashboard

---
+++ configuration/pih/statusData/mentalHealth/lastDisposition.sql
@@ -0,0 +1,17 @@
+select concept_from_mapping('PIH', '8620')
+into @dispositionCategory;
+
+
+
+# Get the latest obs_id with dispositionCategory 
+SELECT o.value_coded  into @valueCoded
+FROM obs o
+join encounter e on e.encounter_id = o.encounter_id
+where o.person_id = @patientId
+and o.concept_id = @dispositionCategory
+and o.voided = 0
+order by o.obs_datetime desc
+limit 1;
+
+select
+    @valueCoded as disposition;
