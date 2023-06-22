6fc3b6eb770920630a37ebbc1691be376306f210
dmdesimone <ddesimone@pih.org>
2023-06-22 12:55:21 -0400

    Update tb_prophylaxis_order_reason.sql

--- configuration/pih/migrations/tb_prophylaxis_order_reason.sql
+++ configuration/pih/migrations/tb_prophylaxis_order_reason.sql
@@ -3,7 +3,7 @@
 -- with at least one of the 3 drugs used in cocktails with it
 -- these are updated with an order reason = "TB Prophylaxis"
 
-drop temporary table if exists temp_tb_prophylaxis_orders;
+drop temporary table if exists temp_tb_prophylaxis_orders; -- 26814 vs
 create temporary table temp_tb_prophylaxis_orders
 select o.order_id 
 from orders o
@@ -17,10 +17,15 @@ and not EXISTS
 	and o2.date_activated <= COALESCE(o.date_stopped,auto_expire_date)
 	and o2.date_stopped >= COALESCE(o.date_stopped,auto_expire_date)
 	and o2.order_action = 'NEW' 
-	and o2.concept_id IN 
-		(concept_from_mapping('PIH','RIFAMPICIN'),
-		concept_from_mapping('PIH','ETHAMBUTOL'),
-		concept_from_mapping('PIH','PYRAZINAMIDE')) 
+		and o2.concept_id IN 
+			(concept_from_mapping('PIH','RIFAMPICIN'),
+			concept_from_mapping('PIH','ETHAMBUTOL'),
+			concept_from_mapping('PIH','PYRAZINAMIDE'),
+			concept_from_mapping('PIH','RIFAMPICIN AND ISONIAZID'),
+			concept_from_mapping('PIH','RIFAMPICIN ISONIAZID AND ETHAMBUTOL'),
+			concept_from_mapping('PIH','RIFAMPICIN ISONIAZID AND PYRAZINAMIDE'),
+	 	    concept_from_mapping('PIH','ETHAMBUTOL AND ISONIZAID')
+		)
 	); 
 
 update orders o
