6d856cc0e954d68abebc3ad98e4a6cd734df2110
Dutervil wadson <44613004+Dutervil@users.noreply.github.com>
2023-12-01 14:18:50 -0500

    fixing locale issue (#276)
    * fixing locale issue
    
    * translating On Arv
    
    ---------
    
    Co-authored-by: =Dutervil <=dwadson3@gmail.com>

--- configuration/pih/statusData/hiv/hivPatientStatus.sql
+++ configuration/pih/statusData/hiv/hivPatientStatus.sql
@@ -14,12 +14,13 @@ select program_id into @hivProgramId from program where uuid='b1cb1fc1-5190-4f7a
 select  programStartDate( patientProgramId(@patientId, @hivProgramId,NOW())) into @enrollmentDate;
 select program_workflow_id  into @hivTreatmentWorkflow  from program_workflow pw where concept_id = concept_from_mapping('PIH','11449') and retired = 0;
 
-select currentProgramState(patientProgramId(@patientId, @hivProgramId,NOW()), @hivTreatmentWorkflow,'en') into @currentStatus;
+select currentProgramState(patientProgramId(@patientId, @hivProgramId,NOW()), @hivTreatmentWorkflow,@locale) into @currentStatus;
 
-SELECT 
+-- noinspection SqlDialectInspection
+SELECT
     CASE 
-        WHEN @patientProgramDate is not null or @outcomeConceptId is not null then concept_name(@outcomeConceptId,'en')
-        WHEN DATEDIFF(NOW(), COALESCE(@nextDispensingDate,@enrollmentDate)) <= 28 THEN 'active - on arvs' 
+        WHEN @patientProgramDate is not null or @outcomeConceptId is not null then concept_name(@outcomeConceptId,@locale)
+        WHEN DATEDIFF(NOW(), COALESCE(@nextDispensingDate,@enrollmentDate)) <= 28 THEN 'active_on_arvs'
         WHEN DATEDIFF(NOW(), COALESCE(@nextDispensingDate,@enrollmentDate)) >= 28 AND @currentStatus IS NOT NULL THEN @currentStatus
         ELSE 'ltfu' 
     END into @status;
